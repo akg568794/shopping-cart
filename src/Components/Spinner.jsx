ba8dccbad844a9aa6b7e571075f42ac071309c4b
abhay gupta <akg568794@gmail.com>
2024-04-27 19:47:59 +0530

    updated Ui for Cart page

--- src/Components/Spinner.jsx
+++ src/Components/Spinner.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 export default function Spinner() {
   return (
-    <div className='spinner'></div>
+    <div className='grid justify-items-center w-[100%] h-screen'>
+      <div className='spinner'></div>
+    </div>
   )
 }
 
